2bbd90be88b8661dae3dec11e25aa863e6e98f9a
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-06-07 11:56:33 -0300

    criei um vetor com vagas para percorrer ele com o .map e redenrizar o vacancyCard com cada valo específico

---
+++ projeto/src/app/components/vacancyPage.js
@@ -0,0 +1,23 @@
+"use client";
+import Image from "next/image";
+import VacancyCard from "./vacancyCard";
+
+export default function VacancyPage() {
+  const vacanicies = [
+    {
+      id: 1,
+      title: "Pintor",
+      location: "Rua da Paz, Recife",
+      image: "/pintor.jpg",
+    },
+
+    {
+      id: 2,
+      title: "Eletricista",
+      location: "Espinheiro, Recife",
+      image: "/eletricista.jpg",
+    },
+  ];
+
+  return;
+}
